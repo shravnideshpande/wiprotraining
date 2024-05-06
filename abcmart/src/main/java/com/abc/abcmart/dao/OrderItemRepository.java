417b30280122b394d1111ad1dfb7eecead086f68
shravnideshpande <sd.shravni22@gmail.com>
2024-05-06 15:46:20 +0530

    capstone project added

--- abcmart/src/main/java/com/abc/abcmart/dao/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.abc.abcmart.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.abc.abcmart.entity.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem,Integer>{
-
-}
